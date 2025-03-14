70110b105835457ca31e315e7098c188958c3d50
Jay Collett <486430+jaycollett@users.noreply.github.com>
2025-03-14 15:39:03 -0400

    Bumps version to the new refactored codebase

--- sermon_search/__init__.py
+++ sermon_search/__init__.py
@@ -4,4 +4,4 @@ TCF Sermon Searcher - A Flask application for searching sermon transcripts.
 This package contains all the modules and components needed for the sermon search application.
 """
 
-__version__ = "0.3.0"
+__version__ = "0.3.56"
