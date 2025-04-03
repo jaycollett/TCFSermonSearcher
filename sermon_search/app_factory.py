603dd3d642a4424759fbde8940f1e0f404e238c6
Jay Collett <486430+jaycollett@users.noreply.github.com>
2025-04-03 17:00:44 -0400

    Updates client ip logic

--- sermon_search/app_factory.py
+++ sermon_search/app_factory.py
@@ -60,7 +60,7 @@ def create_app(config_name=None):
                 static_folder=static_dir)
     
     # Apply ProxyFix middleware to handle HAProxy layer
-    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=6, x_proto=6)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1)
     
     # Get configuration based on environment
     Config = get_config(config_name)
